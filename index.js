c35cf4eb2d084427ebfeadd5d560ebd6eb626ccc
vilicvane <i@vilic.info>
2016-08-12 18:08:00 +0800

    Add tls support

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 var MqttClient = require('./lib/client.js');
 var MqttServer = require('./lib/server.js').MqttServer;
+var MqttSecureServer = require('./lib/server.js').MqttSecureServer;
 var MqttConnection = require('./lib/connection/index.js');
 var Store = require('./lib/store.js');
 var connect = require('./lib/connect/index.js');
@@ -21,6 +22,8 @@ exports.Store = Store;
 // Expose MqttServer
 exports.Server = MqttServer;
 exports.MqttServer = MqttServer;
+exports.SecureServer = MqttSecureServer;
+exports.MqttSecureServer = MqttSecureServer;
 
 // Expose Connection
 exports.MqttConnection = MqttConnection;
